8ac1c8ff1eaaf59208cb16419c7ea6186ffae424
a <a@MacBook-Pro-de-a.local>
2025-10-20 16:53:22 +0200

    commit final

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Hero from "../components/Hero";
 import AboutAndSkillsSection from '../components/AboutAndSkillsSection';
-import GithubModal from "../components/GithubModal";
 
 
 export default function Home() {
